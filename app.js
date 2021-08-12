7111697c32e3076a8d6f3580b91dc86bb84a04fc
mdihalif <mdihalif@yahoo.com>
2021-08-13 00:32:48 +0600

    php to html

---
+++ app.js
@@ -0,0 +1,17 @@
+setInterval( () => {
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+    let hrotation = 30 * htime + mtime/2;
+    let mrotation = 6 * mtime;
+    let srotation = 6 * stime;
+
+    const hour = document.getElementById("hour");
+    const minute = document.getElementById("minute");
+    const second = document.getElementById("second");
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
